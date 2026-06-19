751b1695e54b4c096e0128a7b5a20e532bf6cd3d
Compass539 <attamakita_asiwaminami@i.softbank.jp>
2026-06-19 16:29:37 +0900

    Initial commit: Todo app backend

---
+++ todos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    category = models.CharField(max_length=50, blank=True)
+    due_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+
